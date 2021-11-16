1a3d93835ef0a72ea539f09ead9db8a836aeb6b5
Charlie <charliesharrington@gmail.com>
2021-11-16 18:37:24 -0500

    Implementing log

---
+++ src/elementary_function.py
@@ -0,0 +1,45 @@
+"""
+This file contains all of the elementary functions for the cs107-BCXY package. It implements the behavior
+of basic functions on the Variable objects that are not dunder methods. Such functions include trigonometric
+functions, logarithms, etcetera.
+"""
+import math
+# from src.variable import Variable
+import sys
+sys.path.append(sys.path[0][:-5] + 'src')
+from variable import Variable
+# TODO: remove path editing, using for development purposes
+
+
+def log(input, base=math.e):
+    """Executes logarithm operation on Variable, int, or float and returns the result.
+
+    Args:
+        var (Variable, int, or float): item to apply logarithm to
+        base (int or float, optional): logarithm base. Defaults to math.e which uses natural logarithm.
+
+    Returns:
+        Variable, int, or float: resulting logarithm value
+
+    Examples
+    --------
+    """
+    # TODO: write examples for docstring
+    if isinstance(input, int) or isinstance(input, float):
+        return math.log(input, base)
+    elif isinstance(input, Variable):
+        if base == 1:
+            # as per math.log standard
+            raise ZeroDivisionError("float division by zero")
+        elif base > 0:
+            # this will still apply when base = math.e because math.log(math.e) == 1
+            # if Variable.val < 0, this will be caught by math.log()
+            return Variable(val = math.log(input.val, base), der = input.der/(input.val * math.log(base)))
+        else:
+            raise ValueError("math domain error")
+    else:
+        raise TypeError(f"must be a real number or Variable object, not {type(input)}")
+
+
+if __name__ == "__main__":
+    print(log('a'))
