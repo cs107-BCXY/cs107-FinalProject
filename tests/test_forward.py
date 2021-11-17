889bd838ea300d14502d29a82c6e1d2a1dc1c82c
Charlie <charliesharrington@gmail.com>
2021-11-17 18:55:18 -0500

    Creating forward mode test suite

---
+++ tests/test_forward.py
@@ -0,0 +1,12 @@
+import unittest
+from src.variable import Variable
+from src.elementary_functions import *
+from src.forward import Forward
+
+
+class TestForward(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
