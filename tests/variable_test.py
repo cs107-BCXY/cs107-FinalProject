dce97fc3edd7047d68797cbfe372841b3bc97353
Yaxin Lei <sunny_lei@berkeley.edu>
2021-11-18 07:14:45 -0500

    unit tests for variables_test

--- tests/variable_test.py
+++ tests/variable_test.py
@@ -5,12 +5,52 @@ from variable import Variable
 
 
 class TestVariable(unittest.TestCase):
-    def test_construction(self):
-        """Check that Variable is initialized correctly.
+
+    def test_property(self):
+        """
+        Check that Variable is initialized correctly.
         """
         x = Variable(3)
         self.assertEqual(x.val, 3)
         self.assertEqual(x.der, 1)
+
+    def test_set_val(self):
+        """
+        Testing setting val function.
+        """
+        x = Variable(3)
+        x.val = 5
+        self.assertEqual(x.val, 5)
+
+    def test_set_der(self):
+        """
+        testing setting der function.
+        """
+        x = Variable(3)
+        x.der = 7
+        self.assertEqual(x.der, 7)
+
+    def test_add_radd(self):
+        """
+        testing add and radd function.
+        """
+        x = Variable(3)
+        y = Variable(4)
+        result = x + y
+        self.assertEqual(result.val, 7)
+        self.assertEqual(result.der, 2)
+
+        x = Variable(3)
+        y = 4
+        result = x + y
+        self.assertEqual(result.val, 7)
+        self.assertEqual(result.der, 1)
+
+        x = 3
+        y = Variable(4)
+        result = x + y
+        self.assertEqual(result.val, 7)
+        self.assertEqual(result.der, 1)
 
 
 if __name__ == "__main__":
