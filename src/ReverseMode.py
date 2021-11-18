d5711ed9d58502367ac7e584e68820b9fb9be01a
seanb118 <seanbai@berkeley.edu>
2021-11-18 03:23:26 -0500

    trying to add trig functions to reverse mode file

--- src/ReverseMode.py
+++ src/ReverseMode.py
@@ -12,6 +12,9 @@ class RevMod:
         self.grad = 1
         self.children = []
 
+## basic operations in rev mode
+
+
     def __mul__(self, other):
         try:
             val_mul = self.val * other.val
@@ -20,7 +23,7 @@ class RevMod:
             self.grad = None
             other.children.append((self.val, new_RevMod))
             other.grad = None
-        except AttributeError:
+        except AttributeError: #incase not of class
             val_mul = self.val * other
             new_RevMod = RevMod(val_mul)
             self.children.append((other, new_RevMod))
@@ -42,14 +45,48 @@ class RevMod:
             self.grad = None
         return new_RevMod
 
+    # subtraction: adding "-"
+    def __sub__(self, other):
+        try:
+            val_sub = self.val - other.val
+            new_RevMod = RevMod(val_sub)
+            self.children.append((1, new_RevMod))
+            self.grad = None
+            other.children.append((-1, new_RevMod))
+            other.grad = None
+        except AttributeError:
+            val_sub = self.val - other
+            new_RevMod = RevMod(val_sub)
+            self.children.append((1, new_RevMod))
+            self.grad = None
+
+        return new_RevMod
+    # division
+    def __truediv__(self, other):
+        try:
+            val_div = self.val / other.val
+            new_RevMod = RevMod(val_div)
+            self.children.append(( 1 / other.val, new_RevMod))
+            self.grad = None
+            other.children.append(( - self.val / (other.val ** 2) , new_RevMod))  # need confirmation
+            other.grad = None
+        except AttributeError:
+            val_div = self.val / other
+            new_RevMod = RevMod(val_div)
+            self.children.append(( 1 / other, new_RevMod))
+            self.grad = None
+
+        return new_RevMod
+
+
 
     ### function creating gradient ***
-    
-    def get_gradient(self):
+
+    def gradient(self):
         if self.grad is None:
             grad = 0
             for der, child in self.children:
-                grad += der * child.get_gradient()
+                grad += der * child.gradient()
             self.grad = grad
         return self.grad
 
@@ -65,25 +102,22 @@ class RevMod:
 
     # cosine
     def cos(self):
-        val = np.cos(self.val)
         new_RevMod = RevMod(val)
-        self.children.append((-np.sin(self.val), new_RevMod))
-        self.grad = None
+        self.children.append((-np.sin(self.val), new_RevMod)) # -sinx
         return new_RevMod
 
     # tangent
     def tan(self):
-        val = np.tan(self.val)
+        val = np.tan(val)
         new_RevMod = RevMod(val)
-        self.children.append(( 1/(np.cos(self.val)**2), new_RevMod)) 
+        self.children.append(( 1/(np.cos(self.val)**2), new_RevMod)) # 1/ sec **2 x
         self.grad = None
         return new_RevMod
 
     # sin function
     def sin(self):
-        new_RevMod = RevMod(np.sin(self.val))
-        self.children.append((np.cos(self.val), new_RevMod))
-        self.grad = None
+        new_RevMod = RevMod(np.sin(val))
+        self.children.append((np.cos(self.val), new_RevMod)) #cosx
         return new_RevMod
 
 
