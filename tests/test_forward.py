b62440a1f909ef7583802a7f2eb2685728ccb510
Charlie <charliesharrington@gmail.com>
2021-11-18 16:15:47 -0500

    Adding test for Forward constructor and getter methods

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -5,7 +5,17 @@ from src.forward import Forward
 
 
 class TestForward(unittest.TestCase):
-    pass
+    
+    def test_constructor(self):
+        """Check the constructor with simple function and single variable.
+        Will also check getter methods as they are necessary to test constructor."""
+        x = Variable(3)
+        f = lambda t: t**2
+        fmode = Forward(f, x)
+        func = fmode.func
+        vars = fmode.vars
+        self.assertEqual(f(3), func(3))
+        self.assertEqual([x], vars)
 
 
 if __name__ == "__main__":
