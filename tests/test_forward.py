fba6eb84721f7fc8c3c2b715f71f5cf75a15efd0
Charlie <charliesharrington@gmail.com>
2021-11-18 16:50:51 -0500

    Adding test for AttributeError if 'calculate' has not been called

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -1,4 +1,5 @@
 import unittest
+import copy
 from src.variable import Variable
 from src.elementary_functions import *
 from src.forward import Forward
@@ -11,19 +12,20 @@ class TestForward(unittest.TestCase):
         self.x = Variable(3)
         self.y = Variable(4., 5.)
         self.simple_scalar_func = lambda t: t**2
+        self.fmode1 = Forward(self.simple_scalar_func, self.x)
     
     def test_constructor(self):
         """Check the constructor with simple function and single variable.
-        Will also check getter methods as they are necessary to test constructor."""
-        fmode = Forward(self.simple_scalar_func, self.x)
-        func = fmode.func
-        vars = fmode.vars
+        Will also test Variables and function getter methods as they are
+        necessary to test constructor."""
+        func = self.fmode1.func
+        vars = self.fmode1.vars
         self.assertEqual(self.simple_scalar_func(3), func(3))
         self.assertEqual([self.x], vars)
 
     def test_setter(self):
         """Test the variable and function setters."""
-        fmode = Forward(self.simple_scalar_func, self.x)
+        fmode = copy.deepcopy(self.fmode1)
         func = fmode.func
         vars = fmode.vars
         self.assertEqual(self.simple_scalar_func(3), func(3))
@@ -33,6 +35,14 @@ class TestForward(unittest.TestCase):
         fmode.vars = self.y
         self.assertEqual(newf(4), fmode.func(4))
         self.assertEqual([self.y], fmode.vars)
+
+    def test_attribute_error(self):
+        """Test that Attribute error is raised if 'calculate' method has
+        not been called yet."""
+        with self.assertRaises(AttributeError):
+            self.fmode1.value
+        with self.assertRaises(AttributeError):
+            self.fmode1.derivative
 
 
 if __name__ == "__main__":
