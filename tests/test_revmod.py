d4a36fcf947a82a59ffc9ca56c9b192242e2ffa3
seanb118 <seanbai@berkeley.edu>
2021-12-08 18:42:10 -0500

    error in calling reverse mode and import module fixed

--- tests/test_revmod.py
+++ tests/test_revmod.py
@@ -10,8 +10,8 @@ from src.ReverseMode import RevMod
 ## first Testing addition case
 
 def test_add_func():
-    x = RD(20)
-    y = RD(5)
+    x = RevMod(20)
+    y = RevMod(5)
     summed = x + y
     assertEqual(float(summed.get_value()), 25)
 
